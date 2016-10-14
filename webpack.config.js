a79bc4cdd2cb45b035dbf76d4371b0363a7bb6c7
Chen <xing.suifeng@gmail.com>
2016-10-14 14:14:15 +0800

    hack error on bash on ubuntu on windows

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+// Hack for Ubuntu on Windows: interface enumeration fails with EINVAL, so return empty.
+try {
+  require('os').networkInterfaces()
+} catch (e) {
+  require('os').networkInterfaces = () => ({})
+}
+
 const webpack = require('webpack');
 
 module.exports = {
